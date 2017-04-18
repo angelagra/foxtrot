eddd568694994e2eed86804e6ebc9ebfc198577e
SAULO SINESIO ARAUJO MARQUES <saulo.samarques1@senacsp.edu.br>
2017-04-18 20:15:10 -0300

    foxtrot

--- db/index.php
+++ db/index.php
@@ -5,17 +5,15 @@
 // -----------------------------------------------------------------------------
 // Conectando no Banco de Dados.
 
-$db_host = "10.135.0.53\sqledutsi";
-$db_name = "Kanino";
+// $db_host = "10.135.0.53\sqledutsi";
+// $db_name = "Kanino";
+
+$db_host = "foxtrot-pi.database.windows.net";
+$db_name = "foxtrot";
+
 $user = "TSI";
 $password = "SistemasInternet123";
 $dsn = "Driver={SQL Server}; Server=$db_host; Port=1433; Database=$db_name;";
-
-// $db_host = "foxtrot-pi.database.windows.net";
-// $db_name = "foxtrot";
-// $user = "TSI";
-// $password = "SistemasInternet123";
-// $dsn = "Driver={SQL Server}; Server=$db_host; Port=1433; Database=$db_name;";
 // -----------------------------------------------------------------------------
 
 // Validando Conecção ao Banco de Dados.
