202572c1bffe5e48842cabdf09077dfefe0d4c18
Angela Graciete <angelagraciete84@gmail.com>
2017-05-29 09:35:29 -0300

    Add files via upload

---
+++ pages/default-page/index.body.menu.php
@@ -0,0 +1,19 @@
+</head>
+
+<body>
+  <div class="header-full-page">
+    <header class="header">
+      <ul>
+        <li class="logo-foxtrot"><a href="../menu/"><img src="../../images/logo/logo-80x58.png" class="logo-foxtrot"/></a></li>
+        <li class="logo-foxtrot"></li>
+        <li class="logo-foxtrot"></li>
+        <li class="logo-foxtrot"></li>
+        <li class="logo-foxtrot">
+          <a href="../../dataBase/connect/disconnect.php">Sair</a>
+          <br>
+          <p><?php echo $_SESSION['nomeUsuario']; ?><p>
+        </li>
+      </ul>
+    </header>
+  </div>
+  <div class="container-global">
